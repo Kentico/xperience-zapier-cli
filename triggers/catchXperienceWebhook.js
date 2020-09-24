fb94e8b5142aed59b6422975cc39f9fda602e40c
Eric Dugre <EricD@kentico.com>
2020-09-24 15:00:45 -0400

    Remove domain validation

--- triggers/catchXperienceWebhook.js
+++ triggers/catchXperienceWebhook.js
@@ -1,16 +1,6 @@
 const triggerNoun = 'Catch Xperience Webhook'
 
 const performHook = (z, bundle) => {
-    // Enusre that the request came from the website
-    // The Http-Xperience-Domain header will contain the site domain
-    // and all aliases separated by semicolon
-    const requestDomains = bundle.rawRequest.headers['Http-Xperience-Domain'].split(';');
-    const authorizedDomain = bundle.authData.website.split('/')[2];
-
-    if(requestDomains && !requestDomains.includes(authorizedDomain)) {
-        throw new z.errors.HaltedError('Skipped, domain not matched.');
-    }
-
     return [bundle.cleanedRequest];
 }
 
